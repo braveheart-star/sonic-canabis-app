8e3f53b1283a7a2602bed848e66a7e4bf7ac5412
braveheart-star <mouse1996115@gmail.com>
2021-04-16 16:03:05 +0800

    change faviction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import queryString from "query-string";
 
 export default function Home() {
+  const location = useRouter().asPath;
+
+  useEffect(() => {
+    const parsedQuery = queryString.parse(location.substring(1)); // returns the query object
+    console.log("parsedQuery", parsedQuery);
+  });
   return (
     <div className="text-green-400 ">
       <Link href="/auth/login">
