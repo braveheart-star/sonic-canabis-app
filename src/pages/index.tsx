63cad46447f3d6b649561e2ef919bb30c1eead54
braveheart-star <mouse1996115@gmail.com>
2021-04-19 04:03:33 +0800

    finish desktop header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Layout } from "../components/Layout";
 import { getAccessToken } from "../lib/auth";
 
 export default function Home() {
@@ -6,12 +7,14 @@ export default function Home() {
   console.log("🚀 ~ file: index.tsx ~ line 6 ~ Home ~ data", data);
 
   return (
-    <div className="text-green-400 ">
-      <Link href="/auth/login">
-        <button className="items-center px-2 py-1 mr-4 text-sm text-gray-300 underline ">
-          Home
-        </button>
-      </Link>
-    </div>
+    <Layout>
+      <div className="text-green-400 ">
+        <Link href="/auth/login">
+          <button className="items-center px-2 py-1 mr-4 text-sm text-gray-300 underline ">
+            Home
+          </button>
+        </Link>
+      </div>
+    </Layout>
   );
 }
