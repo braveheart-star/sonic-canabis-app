e0d2457cae303a76e3154cd8ca1935a2cdad9aa0
braveheart-star <mouse1996115@gmail.com>
2021-04-28 06:14:07 +0800

    change server_api

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,2 +1,1 @@
-// export const SERVER_BASE_URL = "https://canabismap.com/api";
-export const SERVER_BASE_URL = "http://127.0.0.1:4000/api";
+export const SERVER_BASE_URL = "https://canabismap.com/api";
