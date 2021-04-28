6b9c55ce7000437eb0297c98e77f40bcbf41fff8
braveheart-star <mouse1996115@gmail.com>
2021-04-28 18:40:55 +0800

    add more brands data, built pages

---
+++ src/components/brands/Discover.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Image from "next/image";
+
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import { renderStar } from "../../pages/delivery";
+import { EmptyHeartIcon } from "../../components/icons/EmptyHeartIcon";
+import { brandMockData, flowerMockData, vapeMockData } from "../../utils/data";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+    centerMode: true,
+    paritialVisibilityGutter: 40,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 640 },
+    items: 3,
+    partialVisibilityGutter: 30,
+  },
+
+  mobile: {
+    breakpoint: { max: 640, min: 300 },
+    items: 1,
+    partialVisibilityGutter: 10,
+  },
+};
+
+export const Discover = () => {
+  return (
+    <div>
+      <div className="p-4 space-y-2">
+        <p className="text-lg font-bold text-gray-700 ">Featured Brands</p>
+        {renderBrands(brandMockData)}
+      </div>
+      <div className="p-4 space-y-2">
+        <>
+          <p className="text-lg font-bold text-gray-700 ">Vape Pens</p>
+          <p className="text-sm text-gray-500 ">
+            A handheld, battery-powered device used to consume vaporized canabis
+            concentrate.
+          </p>
+        </>
+        {renderBrands(vapeMockData)}
+      </div>
+      <div className="p-4 space-y-2">
+        <>
+          <p className="text-lg font-bold text-gray-700 ">Flower</p>
+          <p className="text-sm text-gray-500 ">
+            Cannabis flower is the smokable or vapable, trichome-covered part of
+            the cannabis plant.
+          </p>
+          {renderBrands(flowerMockData)}
+        </>
+      </div>
+    </div>
+  );
+};
+
+function renderBrands(mockData: any) {
+  return (
+    <Carousel responsive={responsive} infinite={true}>
+      {mockData.map((item: any, idx: number) => {
+        return (
+          <div className="w-full h-full p-2 mx-auto text-sm " key={idx}>
+            <div className="h-full p-4 space-y-1 border rounded">
+              <div className="w-full">
+                <Image
+                  src={item.imgUrl}
+                  alt="slide"
+                  width="1000"
+                  height="800"
+                  draggable="false"
+                />
+              </div>
+              <p className="text-base font-bold text-gray-700 ">{item.title}</p>
+              <div className="flex items-center space-x-2 text-gray-600 ">
+                {renderStar(item.rate)}
+                <p>{item.rate}</p>
+                <p>({item.member})</p>
+              </div>
+              <p>{item.usage}</p>
+              <p>{item.price}</p>
+            </div>
+          </div>
+        );
+      })}
+    </Carousel>
+  );
+}
--- src/pages/brand/index.tsx
+++ src/pages/brand/index.tsx
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-
 import { Layout } from "../../components/common/Layout";
-import { brandMockData } from "../../utils/data";
-import { EmptyHeartIcon } from "../../components/icons/EmptyHeartIcon";
+import { Discover } from "../../components/brands/Discover";
 
 const brands = [
   {
@@ -49,26 +44,6 @@ const brands = [
   },
 ];
 
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 5,
-    centerMode: true,
-    paritialVisibilityGutter: 40,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 640 },
-    items: 3,
-    partialVisibilityGutter: 30,
-  },
-
-  mobile: {
-    breakpoint: { max: 640, min: 300 },
-    items: 1,
-    partialVisibilityGutter: 10,
-  },
-};
-
 export default function brand() {
   return (
     <Layout>
@@ -77,46 +52,7 @@ export default function brand() {
           <RenderProvider />
         </div>
         <div className="container w-full p-4 mx-auto mt-4 space-y-4 text-gray-700 bg-white rounded shadow-sm lg:mt-8 max-w-7xl">
-          <div>
-            <p className="p-4 text-lg font-bold text-gray-700 ">
-              Featured Brands
-            </p>
-            <Carousel responsive={responsive} infinite={true}>
-              {brandMockData.map((item, idx) => {
-                return (
-                  <div className="w-full h-full p-2 mx-auto text-sm " key={idx}>
-                    <div className="relative h-full p-4 space-y-1 border rounded">
-                      <div className="w-full">
-                        <Image
-                          src={item.imgUrl}
-                          alt="slide"
-                          width="1000"
-                          height="800"
-                          draggable="false"
-                        />
-                      </div>
-                      <p className="text-base font-semibold text-gray-700 ">
-                        {item.title}
-                      </p>
-                      <p className="p-1 ">
-                        {item.flowerNumber}
-                        <span className="mx-2">Followers</span>
-                      </p>
-
-                      <div className="absolute top-6 right-6">
-                        <button className="p-1 bg-white border rounded-full w-7 h-7">
-                          <EmptyHeartIcon className="w-full h-full" />
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </Carousel>
-          </div>
-          <div>
-            <p className="p-4 text-lg font-bold text-gray-700 ">Vape Pens</p>
-          </div>
+          <Discover />
         </div>
       </div>
     </Layout>
