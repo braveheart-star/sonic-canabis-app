e101d301acc855c2114721267c87a71698d3fe26
braveheart-star <mouse1996115@gmail.com>
2021-05-05 00:15:53 +0800

    remove files, refactored layout

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,24 +1,15 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
 export const Layout = ({ children }: any) => {
   return (
     <div>
-      <div className="relative flex flex-col w-full min-h-screen ">
-        <motion.main
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <Header />
-          <div className="flex min-h-screen py-4 bg-gray-100 lg:py-8 ">
-            {children}
-          </div>
-          <Footer />
-        </motion.main>
+      <Header />
+      <div className="flex min-h-screen py-4 bg-gray-100 lg:py-8 ">
+        {children}
       </div>
+      <Footer />
     </div>
   );
 };
